f27361601ea74233d9461d61183b6e12b23626ba
Tyler Vernon <tvernon@nearinfinity.com>
2013-02-27 16:39:26 -0500

    Reads from standard in, attempts to find start of frames, and prints out a line only if it contains the start of a frame.

--- src/tshark2json.cpp
+++ src/tshark2json.cpp
@@ -2,33 +2,37 @@
 #include <regex.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main(int argc, char* argv[]) {
   regex_t regexFrame;
-  const char* s = "Frame 43: 54 bytes on wire (432 bits), 54 bytes captured (432 bits)";
   
-  char* buffer = (char*)malloc(1000);
-  size_t n = 1000;
+  string line;
+  size_t n = 1;
   ssize_t read;
-  while((read = getline(&buffer, &n, stdin)) != -1) {
-      printf("line: %d %s", n, buffer);
-      n = 1000;
+  while(getline(cin, line)) {
+      n++;
+      regcomp(&regexFrame, "Frame [0-9]*:", 0);
+  
+      size_t nmatch = 2;
+      regmatch_t pmatch[2];
+      int ret = regexec(&regexFrame, line.c_str(), nmatch, pmatch, 0);
+      if(!ret) {
+        // printf("match %d\n", ret);
+        // printf("With the whole expression, "
+        //          "a matched substring \"%.*s\" is found at position %d to %d.\n",
+        //          pmatch[0].rm_eo - pmatch[0].rm_so, &line.c_str()[pmatch[0].rm_so],
+        //          pmatch[0].rm_so, pmatch[0].rm_eo - 1);
+              printf("line: %d %s\n", n, line.c_str());
+
+      } else {
+        // printf("no match %d\n", ret);
+      }        
   }
   
-  regcomp(&regexFrame, "Frame [0-9]*:", 0);
   
-  size_t nmatch = 2;
-  regmatch_t pmatch[2];
-  int ret = regexec(&regexFrame, s, nmatch, pmatch, 0);
-  if(!ret) {
-    printf("match %d\n", ret);
-    printf("With the whole expression, "
-             "a matched substring \"%.*s\" is found at position %d to %d.\n",
-             pmatch[0].rm_eo - pmatch[0].rm_so, &s[pmatch[0].rm_so],
-             pmatch[0].rm_so, pmatch[0].rm_eo - 1);
-  } else {
-    printf("no match %d\n", ret);
-  }
   
   regfree(&regexFrame);
   
