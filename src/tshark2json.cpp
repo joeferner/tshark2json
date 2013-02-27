e3844c5f81e539d26e61f725b59081fc439ea3fa
Joe Ferner <joe@fernsroth.com>
2013-02-27 16:10:46 -0500

    regex

--- src/tshark2json.cpp
+++ src/tshark2json.cpp
@@ -1,7 +1,27 @@
 
-#include <stdio.h>
+#include <string>
+#include <regex.h>
 
 int main(int argc, char* argv[]) {
-  printf("Hello\n");
+  regex_t regexFrame;
+  const char* s = "Frame 43: 54 bytes on wire (432 bits), 54 bytes captured (432 bits)";
+  
+  regcomp(&regexFrame, "Frame [0-9]*:", 0);
+  
+  size_t nmatch = 2;
+  regmatch_t pmatch[2];
+  int ret = regexec(&regexFrame, s, nmatch, pmatch, 0);
+  if(!ret) {
+    printf("match %d\n", ret);
+    printf("With the whole expression, "
+             "a matched substring \"%.*s\" is found at position %d to %d.\n",
+             pmatch[0].rm_eo - pmatch[0].rm_so, &s[pmatch[0].rm_so],
+             pmatch[0].rm_so, pmatch[0].rm_eo - 1);
+  } else {
+    printf("no match %d\n", ret);
+  }
+  
+  regfree(&regexFrame);
+  
   return 0;
 }
